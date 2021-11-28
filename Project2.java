6f0378420071d3b05ec5987d7d5608565d20ba03
vaibhavi8 <vaibjhawar@gmail.com>
2021-11-28 00:13:54 -0800

    added comments explaining FloydWarshall

--- Project2.java
+++ Project2.java
@@ -2,9 +2,10 @@ import java.util.*;
 //import java.lang.*;
 public class Project2{
     public static void main(String[]args){
-        int[][] graph = { {0,7,12,100,100,100}, {100,0,2,9,100,100}, {100,100,0,100,10,100}, {100,100,100,0,100,1}, {100,100,100,4,0,5}, {100,100,100,100,100,0} };
+        //the verticies that have no paths to other vertices are considered to be 1000 (a positive big number representing infinite) instead of 0
+        int[][] graph = { {0,7,12,1000,1000,1000}, {1000,0,2,9,1000,1000}, {1000,1000,0,1000,10,1000}, {1000,1000,1000,0,1000,1}, {1000,1000,1000,4,0,5}, {1000,1000,1000,1000,1000,0} };
         int [][] graphB = 
-        {{0,3,100,7},{8,0,2,100},{5, 100, 0, 1},{2, 100, 100, 0}};
+        {{0,3,1000,7},{8,0,2,1000},{5, 1000, 0, 1},{2, 1000, 1000, 0}};
         for(int i = 0; i < 6; i++){
             System.out.println(Arrays.toString(graph[i]));
         }
@@ -39,7 +40,8 @@ public class Project2{
         int[][] prev = new int[n][n];
         int [][] curr = new int[n][n];
         prev = a;
-        for(int i = 0; i < prev.length; i++){
+        //prints out the graph, and ensures that graph prev replicates a 
+        for(int i = 0; i < prev.length; i++){  
             for(int j =0; j<prev[0].length;j++){
                 System.out.print(prev[i][j] + " ");
             }
@@ -47,14 +49,20 @@ public class Project2{
         }
         System.out.println();
 
+        //creates k iterations of the graph, each iteration keeps the previous graph's row/column k
+        //number of iterations is the number of verticies
         for(int k = 0; k < n; k++){
             for(int i = 0; i < n ; i++){
                 for(int j=0; j < n; j++){
+                    //goes through the graph, the current graph selects the shortest path by comparing the current path to the path that has to go through vertice k
+                    //the new shortest path is set as current, and the algorithm continues to check paths through all the verticies to get the overall shortest path (by iterating through k)
                     curr[i][j] = Math.min(prev[i][j], prev[i][k]+prev[k][j]);
                 }
             }
+            //the previous graph is set to the current graph to continue comparisons and find the shortest path
             prev = curr;
         }
+        //prints out the shortest paths for the directed weighted graphs
         for(int i = 0; i < curr.length; i++){
             for(int j = 0; j<curr[0].length;j++){
                 System.out.print(curr[i][j] + " ");
